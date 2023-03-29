3bfbe067ff3e8b78d92107a3ff0045abc2819e01
MaciejMondalski <maciej.mondalski@gmail.com>
2023-03-29 23:41:55 +0200

    added login styles

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -5,7 +5,10 @@ import { signIn } from 'next-auth/react';
 export default function Login() {
   return (
     <li className="list-none">
-      <button onClick={() => signIn()} className="tx-sm">
+      <button
+        onClick={() => signIn()}
+        className="tx-sm bg-gray-700 text-white py-2 px-6 rounded-xl disabled:opacity-25"
+      >
         Sign In
       </button>
     </li>
