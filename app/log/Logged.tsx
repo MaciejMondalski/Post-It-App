742dc0608d399bec8891335e2ed5f46c189fd551
MaciejMondalski <maciej.mondalski@gmail.com>
2023-04-06 23:33:39 +0200

    .

--- app/log/Logged.tsx
+++ app/log/Logged.tsx
@@ -5,5 +5,5 @@ import { signOut } from 'next-auth/react';
 import Link from 'next/link';
 
 export default function Logged() {
-  return <li></li>;
+  return <li className="flex gap-8 items-center"></li>;
 }
