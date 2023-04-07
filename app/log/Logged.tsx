462f4f14c7875bd5a5810ae4f2387ffad92b3c38
MaciejMondalski <maciej.mondalski@gmail.com>
2023-04-07 23:15:31 +0200

    created the sign-out feature

--- app/log/Logged.tsx
+++ app/log/Logged.tsx
@@ -2,8 +2,13 @@
 
 import Image from 'next/image';
 import { signOut } from 'next-auth/react';
-import Link from 'next/link';
 
 export default function Logged() {
-  return <li className="flex gap-8 items-center"></li>;
+  return (
+    <li className="flex gap-8 items-center">
+      <button onClick={() => signOut()} className="bg-gray-700 text-white text-sm px-6 py-2 rounded-md">
+        Sign out
+      </button>
+    </li>
+  );
 }
