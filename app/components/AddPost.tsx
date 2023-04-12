a7f693426578e597b80fde8102b45f16e86aa185
MaciejMondalski <maciej.mondalski@gmail.com>
2023-04-12 10:24:45 +0200

    .

---
+++ app/components/AddPost.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useState } from "react";
+
+export default function CreatePost() {
+  const [title, setTitle] = useState("");
+
+  return (
+    <form>
+      <div>
+        <textarea
+          onChange={(e) => setTitle(e.target.value)}
+          name="title"
+          value={title}
+          placeholder="What's on your mind?"
+        ></textarea>
+      </div>
+    </form>
+  );
+}
