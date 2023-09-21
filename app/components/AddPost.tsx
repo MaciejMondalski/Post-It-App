0380fb7bf88a1209eebd1192d4aa8d843236317d
MaciejMondalski <maciej.mondalski@gmail.com>
2023-09-21 18:20:38 +0200

    created a component for adding posts

--- app/components/AddPost.tsx
+++ app/components/AddPost.tsx
@@ -2,18 +2,33 @@
 
 import { useState } from "react";
 
-export default function CreatePost() {
+export default function AddPost() {
   const [title, setTitle] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
 
   return (
-    <form>
-      <div>
+    <form className="bg-white my-8 p-8 rounded-md">
+      <div className="flex flex-col my-4">
         <textarea
           onChange={(e) => setTitle(e.target.value)}
           name="title"
           value={title}
           placeholder="What's on your mind?"
+          className="p-4 text-lg rounded-md my-2 bg-gray-200"
         ></textarea>
+      </div>
+      <div className="flex items-center justify-between gap-2">
+        <p
+          className={`font-bold text-sm ${title.length > 300 ? "text-red-700" : "text-gray-700"}`}
+        >{`${title.length}/300`}</p>
+        <div>
+          <button
+            disabled={isDisabled}
+            className="tx-sm bg-teal-600 text-white py-2 px-6 rounded-lg disabled:opacity-25"
+          >
+            Create a post
+          </button>
+        </div>
       </div>
     </form>
   );
