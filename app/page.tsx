26a97ff460ac1874014d2157970654d47a7f4b79
MaciejMondalski <maciej.mondalski@gmail.com>
2023-09-25 10:10:05 +0200

    display posts on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,33 @@
 "use client";
-
+import axios from "axios";
 import AddPost from "./components/AddPost";
+import { useQuery } from "@tanstack/react-query";
+import Post from "./components/Post";
+import { PostType } from "./types/Posts";
+
+//Fetch all posts
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery<PostType[]>({ queryFn: allPosts, queryKey: ["posts"] });
+  if (error) return error;
+  if (isLoading) return "Loading...";
   return (
     <main>
-      <h1>Main Page Placeholder</h1>
       <AddPost />
+      {data?.map((post) => (
+        <Post
+          comments={post.comment}
+          key={post.id}
+          id={post.id}
+          name={post.user.name}
+          avatar={post.user.image}
+          postTitle={post.title}
+        />
+      ))}
     </main>
   );
 }
