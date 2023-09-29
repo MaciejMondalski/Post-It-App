2ac39a342d3ec1aad5a849c82b2965b411ead235
MaciejMondalski <maciej.mondalski@gmail.com>
2023-09-29 19:35:52 +0200

    .

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Post({ avatar, name, postTitle, id, comments }) {
+export default function Post({ avatar, name, postTitle, id, comments }: any) {
   console.log(comments);
   return (
     <div className="bg-white my-8 p-8 rounded-lg">
