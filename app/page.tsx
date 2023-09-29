d94b9af1b838203f40294c6ecf99a175ec2ae47e
MaciejMondalski <maciej.mondalski@gmail.com>
2023-09-29 17:38:12 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import AddPost from "./components/AddPost";
 import { useQuery } from "@tanstack/react-query";
 import Post from "./components/Post";
-import { PostType } from "./types/Posts";
+import { PostsType } from "./types/Posts";
 
 //Fetch all posts
 const allPosts = async () => {
@@ -12,9 +12,12 @@ const allPosts = async () => {
 };
 
 export default function Home() {
-  const { data, error, isLoading } = useQuery<PostType[]>({ queryFn: allPosts, queryKey: ["posts"] });
+  const { data, error, isLoading } = useQuery<PostsType[]>({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
   if (error) return error;
-  if (isLoading) return "Loading...";
+  if (isLoading) return "Loading.....";
   return (
     <main>
       <AddPost />
