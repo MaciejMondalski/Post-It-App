bfca6c0ad304f9a374b27e8a63b1b3975a556013
MaciejMondalski <maciej.mondalski@gmail.com>
2023-09-29 16:38:56 +0200

    created comment functionality

---
+++ app/post/[slug]/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import AddComment from "@/app/components/AddComment";
+import Post from "@/app/components/Post";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Image from "next/image";
+
+type URL = {
+  params: {
+    slug: string;
+  };
+};
+
+const fetchDetails = async (slug: string) => {
+  const response = await axios.get(`/api/posts/${slug}`);
+  return response.data;
+};
+
+export default function PostDetail(url: URL) {
+  const { data, isLoading } = useQuery({
+    queryKey: ["detail-post"],
+    queryFn: () => fetchDetails(url.params.slug),
+  });
+  if (isLoading) return "Loading...";
+  console.log(data);
+
+  return (
+    <div>
+      <Post
+        id={data.id}
+        name={data.user.name}
+        avatar={data.user.image}
+        postTitle={data.title}
+        comments={data.comment}
+      />
+      <AddComment id={data?.id} />
+      {data?.comment?.map((comment) => (
+        <div className="my-6 bg-white p-8 rounded-md">
+          <div className="flex items-center gap-2">
+            <Image
+              className="rounded-full"
+              key={comment.id}
+              width={32}
+              height={32}
+              src={comment.user?.image}
+              alt="avatar"
+            />
+            <h3 className="font-bold">{comment?.user?.name}</h3>
+            <h3 className="text-sm text-gray-400">{comment.createdAt}</h3>
+          </div>
+          <div className="py-4 text-gray-700">{comment.title}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
